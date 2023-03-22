4e659ab29cddf561cc7ac50978182b626213657b
fygure <92011471+fygure@users.noreply.github.com>
2023-03-22 15:53:37 -0500

    folder structure

---
+++ server/data.js
@@ -0,0 +1,19 @@
+//data.js
+// Temporary data
+const users = [
+    {
+        id: 1,
+        username: "Max44",
+        password: "password",
+        type: "admin",
+    },
+    {
+        id: 2,
+        username: "Sam99",
+        password: "password",
+        type: "customer",
+    },
+];
+
+
+module.exports = users;
