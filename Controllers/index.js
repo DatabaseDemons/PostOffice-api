9a1869f262c438963a489c3957488594767547b7
fygure <92011471+fygure@users.noreply.github.com>
2023-03-25 21:34:51 -0500

    removed index js files

--- Controllers/index.js
+++
@@ -1,7 +0,0 @@
-//import all controllers here
-
-
-
-
-
-//export all controllers here (to be read from app.js)
--- Models/index.js
+++
@@ -1,7 +0,0 @@
-//import all models here
-
-
-
-
-
-//export all models here (to be read from app.js)
