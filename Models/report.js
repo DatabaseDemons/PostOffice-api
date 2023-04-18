97f542503d1c7e12c789d6dd1215cbfc388815ae
Gordon Nguyen <maixanh45@yahoo.com>
2023-04-18 00:50:42 -0500

    Added employee report

--- Models/report.js
+++ Models/report.js
@@ -1,6 +1,39 @@
 const { client } = require('./db');
 
 class Report {
+    static async getEmployeeReport(filter) {
+        try {
+            console.log(filter)
+            const newFilter = JSON.parse(filter);
+            console.log(newFilter); // Fixme remove printout before prod
+
+            const start_date = newFilter.start_date;
+            const end_date = newFilter.end_date;
+
+            const report = await client.query(`
+            SELECT first_name, last_name, work_name, pay, hours_worked, on_date, branch_address, phone_number, email, start_date, is_employed
+            FROM postoffice.EMPLOYEE e
+            INNER JOIN postoffice.JOB_HISTORY jh ON jh.employee_email = e.email
+            WHERE on_date BETWEEN '${start_date}' AND '${end_date}';`
+            );
+
+            const summary = await client.query(`
+            SELECT COUNT(DISTINCT e.email) AS employee_count, SUM(jh.pay) AS total_pay, SUM(hours_worked) AS total_hrs, COUNT(jh.work_id) AS total_job
+            FROM postoffice.EMPLOYEE e, postoffice.JOB_HISTORY jh 
+            WHERE jh.employee_email = e.email AND jh.on_date BETWEEN '${start_date}' AND '${end_date}';`
+            );
+
+            const result = report.recordset.concat(summary.recordset);
+            console.log(result);
+            return result;
+
+        } catch (err) {
+            console.log(err);
+            throw new Error('Failed to retrieve shipment report.');
+        }
+    }
+
+
     static async getShipmentReport(filter) {
         try {
             console.log(filter)
